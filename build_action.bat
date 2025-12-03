e2400fdbf63ce2a9a6c1aebeac7e416ef5d181ec
Waim908 <waimxj449@foxmail.com>
2025-12-03 11:24:47 +0800

    test

--- build_action.bat
+++ build_action.bat
@@ -2,6 +2,4 @@ if not defined NUMBER_OF_PROCESSORS set NUMBER_OF_PROCESSORS=8
 if exist wfm.exe del wfm.exe
 if exist obj del /s /q obj
 set PATH=C:\w64devkit\bin
-make -j%NUMBER_OF_PROCESSORS%
-start wfm.exe
-pause
+make -j4
