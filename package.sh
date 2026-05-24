98440346bc30f811aca815b3416eaa0fe8c79b38
Waim908 <waimxj449@foxmail.com>
2026-05-24 21:48:04 +0800

    Update package.sh

--- package.sh
+++ package.sh
@@ -1,9 +1,9 @@
 case $1 in
 	nolibdio)
-		7z a -tzip wfm.zip wfm.exe libcdio.dll del-reg.bat wfm-extra.bat install-winlator.bat
+		7z a -tzip wfm-nolibcdio.zip wfm.exe del-reg.bat wfm-extra.bat install-winlator.bat
 	;;
 	*)
-		7z a -tzip wfm-nolibcdio.zip wfm.exe del-reg.bat wfm-extra.bat install-winlator.bat
+		7z a -tzip wfm.zip wfm.exe libcdio.dll del-reg.bat wfm-extra.bat install-winlator.bat
 	;;
 esac
 
