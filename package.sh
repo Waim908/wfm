1789e5cdc5804c03c4c03e864646677d571ae21a
Waim908 <waimxj449@foxmail.com>
2026-05-24 20:09:46 +0800

    1.5-2.2: Update package.sh

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@ case $1 in
 		7z a -tzip wfm.zip wfm.exe libcdio.dll del-reg.bat wfm-extra.bat install-winlator.bat
 	;;
 	nolibcdio)
-		7z a -tzip wfm.zip wfm.exe del-reg.bat wfm-extra.bat install-winlator.bat
+		7z a -tzip wfm-nolibcdio.zip wfm.exe del-reg.bat wfm-extra.bat install-winlator.bat
 	;;
 esac
 
