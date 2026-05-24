df787edd0de21fc99883f9316c38967cd505b5fb
Waim908 <waimxj449@foxmail.com>
2026-05-24 20:09:11 +0800

    1.5-2.2: Update package.sh & bat

--- install-winlator.bat
+++ install-winlator.bat
@@ -1,6 +1,9 @@
 taskkill /f /im wfm.exe
 copy /y .\wfm.exe C:\windows
-copy /y .\libcdio.dll C:\windows
+if exist ".\libcdio.dll" (
+    echo "Copying libcdio.dll"
+    copy /y ".\libcdio.dll" C:\windows
+)
 
 echo "Reboot To Apply Update!"
 
--- package.sh
+++ package.sh
@@ -1,1 +1,9 @@
-7z a -tzip wfm.zip wfm.exe libcdio.dll del-reg.bat wfm-extra.bat install-winlator.bat
+case $1 in
+	*)
+		7z a -tzip wfm.zip wfm.exe libcdio.dll del-reg.bat wfm-extra.bat install-winlator.bat
+	;;
+	nolibcdio)
+		7z a -tzip wfm.zip wfm.exe del-reg.bat wfm-extra.bat install-winlator.bat
+	;;
+esac
+
