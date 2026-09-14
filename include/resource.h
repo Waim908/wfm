c2fac5b3591c7a6d86a2f25faf23a47518ae4ed3
Waim908 <waimxj449@foxmail.com>
2026-09-14 23:13:21 +0800

    tag: update 1.5-2.9

--- include/resource.h
+++ include/resource.h
@@ -128,7 +128,7 @@ void freeUIcons();
 #endif
 
 #define APP_NAME L"Winlator File Manager"
-#define APP_VERSION L"1.5-mod.2.8"
+#define APP_VERSION L"1.5-mod.2.9"
 #define APP_DEV_NAME L"BrunoSX"
 
 #ifndef RESOURCE_H
